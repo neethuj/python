4e9eeebf4598841a46d40696d282f8e1483c74c9
Neethu <neethubhas010@gmail.com>
2023-02-13 20:11:57 -0600

    Python Lab_Day_1

---
+++ demo.py
@@ -0,0 +1,10 @@
+print("Hello") 
+x = 2 + 2
+print (x)
+y = 4 - 2
+print (y)
+z = 2 * 8
+print (z)
+q = 4 / 2
+print (q)
+print("Python has three numeric types: int, float, and complex")
---
+++ string-data-type.py
@@ -0,0 +1,13 @@
+myString = "This is a String."
+print (myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name = input("what is your name? ")
+print(name)
+color = input("what is your favorite color? ")
+animal = input("what is your favorite animal? ")
+print("{}, you like a {} {}!". format(name,color,animal))
